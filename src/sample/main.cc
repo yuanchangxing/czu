724ffc09cdb6c44b05bf5a672170e108b138ebc2
songtzu <songtzu@126.com>
2017-04-30 00:16:04 +0800

    dev sample

--- src/sample/main.cc
+++ src/sample/main.cc
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "../net/pack_base.h"
 
+#include "../net/net_base.h"
+
+class Server :public NetBase{
+public:
+    void OnRecv(int _sockid, PackBase &_pack) {
+        send(_sockid,_pack);
+    }
+};
+
 int main(int argc, char **argv)
 {
 	/* code */
