9dd8aedf54469131e6e104cc807ef49f4f53dd61
rotencode <songtzu@yeah.net>
2017-05-02 08:36:11 -0700

    dev code clean up.

--- src/sample/main.cc
+++ src/sample/main.cc
@@ -1,27 +1,23 @@
 #include "../net/pack_base.h"
 
 #include "../net/net_base.h"
-#include "clog.h"
 
-#include <stdio.h>
 #include <Python.h>
-#include <iostream>
 
 using namespace czu;
 
 
 //python integrate sample.
 void callpy() {
-    Py_Initialize();              //使用python之前，要调用Py_Initialize();这个函数进行初始化
+    Py_Initialize();                                                //py parser init.
     PyRun_SimpleString("import sys");
     PyRun_SimpleString("sys.path.append('./entry')");
-    PyObject * ptr_module = NULL;    //声明变量
-    PyObject * ptr_func = NULL;      //声明变量
+    PyObject * ptr_module = NULL;                                   //module for py. file name.
+    PyObject * ptr_func = NULL;                                     //function name for py.
     PyObject * ptr_parameters = NULL;
     PyObject * ptr_result = NULL;
     const char* ptr_buffer = NULL;
-    int value = 0;
-//    pParam = Py_BuildValue("(s)", "hi this is buffer");
+    int code = 0;
     ptr_parameters = PyTuple_New(7);
     PyTuple_SetItem(ptr_parameters, 0, Py_BuildValue("i", 7777777));
     PyTuple_SetItem(ptr_parameters, 1, Py_BuildValue("i", 1));
@@ -29,34 +25,31 @@ void callpy() {
     PyTuple_SetItem(ptr_parameters, 3, Py_BuildValue("i", 3));
     PyTuple_SetItem(ptr_parameters, 4, Py_BuildValue("i", 4));
     PyTuple_SetItem(ptr_parameters, 5, Py_BuildValue("i", 5));
-
     PyTuple_SetItem(ptr_parameters, 6, Py_BuildValue("s", "hi this is buffer"));
 
 
 
-
-
-
-    ptr_module =PyImport_ImportModule("main");              //这里是要调用的Python文件名
-    ptr_func= PyObject_GetAttrString(ptr_module, "entry_py");   //这里是要调用的函数名
-    ptr_result = PyEval_CallObject(ptr_func, ptr_parameters);                         //
+    ptr_module =PyImport_ImportModule("main");                      //python module file name.
+    ptr_func= PyObject_GetAttrString(ptr_module, "entry_py");       //python function name.
+    ptr_result = PyEval_CallObject(ptr_func, ptr_parameters);       //call function.
     if(ptr_result) {
-        if(PyArg_ParseTuple(ptr_result, "is",  &value, &ptr_buffer)) {
-            LOGD("value:%d, buffer:%s", value, ptr_buffer);
-//            std::cout <<"value:"<<value<<"ptr_buffer:"<< ptr_buffer << std::endl;
+        if(PyArg_ParseTuple(ptr_result, "is",  &code, &ptr_buffer)) {
+            LOGD("code:%d, buffer:%s", code, ptr_buffer);
         } else{
             LOGE("parse error");
         }
     } else{
         LOGE("python return value ",ptr_result);
     }
-    Py_DECREF(ptr_module);
+
+
+
+    Py_DECREF(ptr_module);                                          //clean up.
     Py_DECREF(ptr_func);
     Py_DECREF(ptr_parameters);
     Py_DECREF(ptr_result);
 
-
-    Py_Finalize();                //调用Py_Finalize,这个和Py_Initialize相对应的.
+    Py_Finalize();
 }
 
 
