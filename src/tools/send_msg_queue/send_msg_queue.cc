a5d1185c50dad7793c63fa2e82ba7c02d21e884e
rotencode <songtzu@yeah.net>
2017-05-23 00:33:55 -0700

    dev, shared lib for python to call while need to write back processed msg.

---
+++ src/tools/send_msg_queue/send_msg_queue.cc
@@ -0,0 +1,4 @@
+//
+// Created by rotencode on 5/22/17.
+//
+
---
+++ src/tools/send_msg_queue/send_msg_queue.h
@@ -0,0 +1,14 @@
+//
+// Created by rotencode on 5/22/17.
+//
+
+#ifndef CCPY_SEND_MSG_QUEUE_H_H
+#define CCPY_SEND_MSG_QUEUE_H_H
+
+
+extern "C" {
+    msg_enqueue();
+}
+
+
+#endif //CCPY_SEND_MSG_QUEUE_H_H
