35bcd822dd8c0dc80c781e2c90f2697841318bcf
rotencode <songtzu@yeah.net>
2017-05-23 20:28:09 -0700

    dev, sample for comsumer&producer,which to send info back to client.

--- src/tools/send_msg_queue/send_msg_queue.cc
+++ src/tools/send_msg_queue/send_msg_queue.cc
@@ -3,8 +3,51 @@
 //
 
 #include "send_msg_queue.h"
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+#include <string>
+#include <thread>
+#include <iostream>
+
+using namespace std;
+std::queue< string > msg_queue;
+
+// synchronization
+std::mutex queue_mutex;
+condition_variable condition;
+
+int task_worker();
 
 
-int msg_enqueue(){
+int msg_enqueue(std::string _msg){
+
+    {
+        std::unique_lock<mutex> lock(queue_mutex);
+        cout <<"msg enqueue" << _msg <<endl;
+        // don't allow enqueueing after stopping the pool
+        msg_queue.push(_msg);
+    }
+    condition.notify_one();
     return 0;
+}
+
+int start_send_task(){
+    std::thread worker(task_worker);
+    worker.detach();
+}
+
+int task_worker() {
+    std::unique_lock<std::mutex> lock(queue_mutex);
+
+    while (true) {
+
+        condition.wait(lock);
+
+        while (!msg_queue.empty()) {
+            std::cout << "consuming " << msg_queue.front() << '\n';
+            msg_queue.pop();
+        }
+
+    }
 }
--- src/tools/send_msg_queue/send_msg_queue.h
+++ src/tools/send_msg_queue/send_msg_queue.h
@@ -5,10 +5,13 @@
 #ifndef CCPY_SEND_MSG_QUEUE_H_H
 #define CCPY_SEND_MSG_QUEUE_H_H
 
+#include <string>
 
 extern "C" {
-    int msg_enqueue();
-}
+    int msg_enqueue(std::string _msg);
 
+    int start_send_task();
+
+}
 
 #endif //CCPY_SEND_MSG_QUEUE_H_H
